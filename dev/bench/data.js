9a961fa9eaa7d5dfc2d268f397b5a6cfba3b9145
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 15:12:22 +0000

    add Benchmark (jmh) benchmark result for 754ce829bf711ed1b8c9f5531c3f4b74f07d9a4c

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779376342320,
+  "repoUrl": "https://github.com/ghasemdev/kodEx",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ghasemdev",
+            "username": "ghasemdev"
+          },
+          "committer": {
+            "name": "ghasemdev",
+            "username": "ghasemdev"
+          },
+          "id": "754ce829bf711ed1b8c9f5531c3f4b74f07d9a4c",
+          "message": "Feature/001 project base setup",
+          "timestamp": "2026-05-18T18:38:16Z",
+          "url": "https://github.com/ghasemdev/kodEx/pull/43/commits/754ce829bf711ed1b8c9f5531c3f4b74f07d9a4c"
+        },
+        "date": 1779376341649,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "dev.kodex.core.model.benchmark.JvmTestBenchmark.cosBenchmark",
+            "value": 105611196.10524487,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.core.model.benchmark.JvmTestBenchmark.sqrtBenchmark",
+            "value": 335336878.25107205,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.server.api.benchmark.JvmTestBenchmark.cosBenchmark",
+            "value": 105249755.60277794,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.server.api.benchmark.JvmTestBenchmark.sqrtBenchmark",
+            "value": 336606430.3716592,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
