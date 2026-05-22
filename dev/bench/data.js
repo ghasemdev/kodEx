07935d524653fcccf22cd10e5c167838efdff430
github-action-benchmark <github@users.noreply.github.com>
2026-05-22 21:38:52 +0000

    add Benchmark (jmh) benchmark result for 557b07fc31f4c50c3e435d8c97334e1f32e29a3c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779482786811,
+  "lastUpdate": 1779485932022,
   "repoUrl": "https://github.com/ghasemdev/kodEx",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "dev.kodex.server.api.benchmark.JvmTestBenchmark.sqrtBenchmark",
             "value": 428871525.36767614,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ghasemdev",
+            "username": "ghasemdev"
+          },
+          "committer": {
+            "name": "ghasemdev",
+            "username": "ghasemdev"
+          },
+          "id": "557b07fc31f4c50c3e435d8c97334e1f32e29a3c",
+          "message": "Feature/001 project base setup",
+          "timestamp": "2026-05-18T18:38:16Z",
+          "url": "https://github.com/ghasemdev/kodEx/pull/43/commits/557b07fc31f4c50c3e435d8c97334e1f32e29a3c"
+        },
+        "date": 1779485931141,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "dev.kodex.core.model.benchmark.JvmTestBenchmark.cosBenchmark",
+            "value": 120291776.80476491,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.core.model.benchmark.JvmTestBenchmark.sqrtBenchmark",
+            "value": 365978023.7198118,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.server.api.benchmark.JvmTestBenchmark.cosBenchmark",
+            "value": 120245046.50289795,
+            "unit": "ops/s",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.kodex.server.api.benchmark.JvmTestBenchmark.sqrtBenchmark",
+            "value": 367138709.537486,
             "unit": "ops/s",
             "extra": "iterations: 1\nforks: 1\nthreads: 1"
           }
